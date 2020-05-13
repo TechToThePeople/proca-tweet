fab00e02bbbd19d552662c3f690143304773234a
xavier.dutoit <xavier@tttp.eu>
2020-05-13 09:30:34 +0200

    with a script to parse twitter lists

---
+++ script/fetchList.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+
+var Twitter = require('twitter');
+
+const config = {
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+};
+var client = new Twitter(config);
+
+var listid = process.argv[2];
+if (!listid) {
+  console.error("you need to give the list id as param\n$node fetchList.js {listid}");
+  process.exit(1);
+}
+
+var params = {list_id: listid, skip_status:true};
+
+function formatProfile (d) {
+  let result = {};
+  try {
+  d.url = d.entities.url.urls[0].expanded_url;
+  } catch (e) {
+    // console.log ("no url"); we skip replacing the url when there isn't one
+  }
+
+  const keys= "id,name,screen_name,location,description,url,profile_image_url_https,followers_count,lang".split(",");  keys.forEach(k => {
+    result[k] = d[k];
+  });
+  return result;
+};
+
+client.get('lists/members', params)
+  .then ( data => {
+    const r = data.users.map (d => formatProfile(d));
+    process.stdout.write(JSON.stringify(r,null,1));
+  })
+  .catch (error => {
+    console.error (error);
+    process.exit(1);
+  });
--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import TwitterAction from './component/TwitterAction.js';
+
 
 function App() {
+  const profiles = [
+     {screenName:"eucampaign",image:"https://proca.foundation/favicon.ico"},
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TwitterAction screenName="eucampaign" />
     </div>
   );
 }
